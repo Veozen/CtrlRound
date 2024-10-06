ab4ad6fc7dd25777b9998e2ec3c19ec67c27f89d
Veozen <152989587+Veozen@users.noreply.github.com>
2024-10-06 09:32:30 -0400

    Update CtrlRound.py

--- CtrlRound.py
+++ CtrlRound.py
@@ -6,12 +6,16 @@ from best_first_search import best_first_search
 
 def CtrlRound(df_in, by, margins, var, roundingBase=1):
 """
+Aggregate a dataframe and perform controlled rounding of it's entries.
 input:
-  df_in       :
-  by          :
-  margins     :
-  var         :
-  roudingBase :
+  df_in       : pandas dataframe
+  by          : list of column names on which to aggregate the input dataframe
+  margins     : list of list of column name indicating which grouping to aggregate. Can be empty in which case all grouping and subgrouping are aggregated.
+  var         : column to be aggregated
+  roudingBase : the rounding base. Has to be greater than 0.
+  
+output:
+  dataframe with columns listed in the "by" and "var" input parameters.
 """
   
   by_to_cellId = {}
