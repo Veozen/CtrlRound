702b795611d433044aeaa832487321d0ac347a3d
Veozen <152989587+Veozen@users.noreply.github.com>
2024-10-14 19:39:41 -0400

    Update CtrlRound.py

--- CtrlRound.py
+++ CtrlRound.py
@@ -1,7 +1,8 @@
 import pandas as pd
-import heapq
+
 import distance
 from best_first_search import best_first_search
+from distance import define_margin_distance, define_interior_distance
 
 def aggBy(df:pd.DataFrame, by, var, id):
     #aggregate a grouped dataframe
@@ -45,6 +46,7 @@ def aggregate_and_list(df:pd.DataFrame, by, margins, var, id):
 
     return df_out  
 
+
 def get_unique_col_name(df,base_name):
   # Generate a unique column name
   i = 1
@@ -68,58 +70,65 @@ input:
 output:
   dataframe with columns listed in the "by" and "var" input parameters.
 """
-  #aggregate "var" by "by" columns in case there are duplicates in the input to make sure we have a table with signle entries per cell
-  by_values = df_in[by].groupby(by).sum(var)
+  # aggregate "var" by "by" columns in case there are duplicates in the input to make sure we have a table with signle entries per cell
+  by_values   = df_in[by].groupby(by).sum(var)
   
   # get a unique name not already present in the dataframe to store cell identifier
-  cellIdName = get_unique_col_name(by_values,"cellId")
+  cellIdName  = get_unique_col_name(by_values,"cellId")
   
-  #create a unique identifer for each cell of the table
-  by_values[cellIdName] = range(len(by_values))
+  # create a unique identifer for each cell of the table
+  by_values[cellIdName]     = range(len(by_values))
   
   # create a mapping of each cell identifer to each value from the table
-  var_values = df_in[[cellIdName,var]].copy()
-  initial_values = {}
+  var_values                = df_in[[cellIdName,var]].copy()
+  initial_values            = {}
   for row in var_values.iterrows():
-    initial_values[row[0]] = row[1]
+    initial_values[row[0]]  = row[1]
   
   # create a mapping of each cell identifer to each possible rounded value 
-  possible_values = var_values
-  lower_residual = possible_values[var]%roundingBase
-  possible_values["lower"] = possible_values[var] - lower_residual
-  possible_values["upper"] = possible_values["lower"]
+  possible_values           = var_values
+  lower_residual            = possible_values[var]%roundingBase
+  possible_values["lower"]  = possible_values[var] - lower_residual
+  possible_values["upper"]  = possible_values["lower"]
   # check if the original value is not already rounded, in which case the upper value should be the same.
   if lower_residual > 0:
-    possible_values["upper"] = possible_values["upper"] + roundingBase
-  possible_cell_values = {}
+    possible_values["upper"]        = possible_values["upper"] + roundingBase
+  possible_cell_values              = {}
   for row in possible_values[[cellIdName,lower,upper]].iterrows():
     # if upper is the same as lower, generate only one possibility
     if row[1] ! = row[2]:
-      possible_cell_values[row[0]] = [row[1],row[2]]
+      possible_cell_values[row[0]]  = [row[1],row[2]]
     else:
-      possible_cell_values[row[0]] = [row[1]]
+      possible_cell_values[row[0]]  = [row[1]]
 
-  #get margins of the input table
-  df_margins  = aggregate_and_list(by_values, by, margins, var, cellIdName)
-  consIdName = get_unique_col_name(df_margins,"consId")
-  df_margins[consIdName] = range(len(df_margins))
+  # get margins of the input table
+  df_margins              = aggregate_and_list(by_values, by, margins, var, cellIdName)
+  consIdName              = get_unique_col_name(df_margins,"consId")
+  df_margins[consIdName]  = range(len(df_margins))
   
   # create a mapping of each margin identifer to each aggregated value 
-  constraint_values = {}
+  constraint_values           = {}
   for row in df_margins[[consIdName,var]].iterrows():
     constraint_values[row[0]] = row[1]
     
   # create a mapping of each margin identifer to a list of each cell identifer adding up to it
-  constraints = {}
+  constraints           = {}
   for row in df_margins[[consIdName,cellIdName]].iterrows():
     constraints[row[0]] = row[1]
 
-  #obtain the best rounding
-  result = best_first_search(possible_cell_values, initial_values, constraints, constraint_values, NSolutions = 1)
+  # define out distances measures
+  calculate_margin_max_distance   = define_margin_distance(max)
+  calculate_margin_sum_distance   = define_margin_distance(sum)
+  calculate_interior_sum_distance = define_interior_distance(sum)
+  distanceFuncs                   = [calculate_margin_max_distance, calculate_margin_sum_distance, calculate_interior_sum_distance]
+
+  # obtain the best rounding
+  result = best_first_search(possible_cell_values, initial_values, constraints, constraint_values, distanceFuncs, NSolutions = 1)
   
-  #assign the rounded values into a dataframe ready for output
-  df_out = by_values.copy()
+  # assign the rounded values into a dataframe ready for output
+  df_out      = by_values.copy()
   df_out[var] = by_values[cellIdName].map(result)
   df_out.drop(cellIdName)
   
   return df_out
+
