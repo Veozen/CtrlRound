53496c0cc1b16739a5b6538f81ea6a6672207746
Veozen <152989587+Veozen@users.noreply.github.com>
2024-10-19 16:13:39 -0400

    Update distance.py

--- distance.py
+++ distance.py
@@ -1,4 +1,4 @@
-    
+
 def define_margin_distance(func, normalized=True):  
   #define the distance function used through the aggregation function used on the margins
   def calculate_margin_distance(partial_solution, initial_values, constraints, constraint_values):
@@ -15,11 +15,10 @@ def define_margin_distance(func, normalized=True):
         target_value = constraint_values[cons]
         current_value = sum(current_values[cell_id] for cell_id in constraints[cons])
         marginDiscrepancies.append(abs(target_value - current_value))  
-      if nCell >1:
+      if nCell >1 and normalized:
         return func(marginDiscrepancies)/nCell
       else:
         return func(marginDiscrepancies)
-
   return calculate_margin_distance
 
 def define_interior_distance(func, normalized=True): 
@@ -30,7 +29,7 @@ def define_interior_distance(func, normalized=True):
       if len(partial_solution) == 0 :
         return 0
       nCell = len(partial_solution) 
-      if nCell >1:
+      if nCell >1 and normalized:
         return func(abs(partial_solution[cell] - initial_values[cell]) for cell in partial_solution)/nCell
       else:
         return func(abs(partial_solution[cell] - initial_values[cell]) for cell in partial_solution)
