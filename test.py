5ee236f2c289b9e15061a9706375e46ba52aacf6
Veozen <152989587+Veozen@users.noreply.github.com>
2024-10-20 18:51:48 -0400

    Update test.py

--- test.py
+++ test.py
@@ -27,3 +27,17 @@ constraint_values = {
 
 result = best_first_search(possible_cell_values, initial_values, constraints, constraint_values, NSolutions = 16)
 print(result)
+
+
+import pandas as pd
+test = pd.DataFrame({
+    "x": [1,2,3,1,2,3],
+    "y": [10,10,20,20,30,30],
+    "id": ["1:10","2:10","3:20","1:20","2:30","3:30"],
+    "z": [11,22,33,44,55,66]
+})
+
+aggBy(test, "x", "z", "id")
+
+test= generate_random_table(3,5,scale=2)
+CtrlRound(test, by=[0,1,2], var="value", roundingBase=1, fixRoundingDist=0.1, maxHeapSize=16000)
