abba519ea1b3522e2a489c2752e1639a9fb33669
Veozen <152989587+Veozen@users.noreply.github.com>
2024-10-21 20:03:41 -0400

    Update CtrlRound.py

--- CtrlRound.py
+++ CtrlRound.py
@@ -70,14 +70,32 @@ def CtrlRound(df_in, by, var, margins=None, roundingBase=1, fixRoundingDist= 0, 
   """
   Aggregate a dataframe and perform controlled rounding of it's entries.
   input:
-    df_in       : pandas dataframe
-    by          : list of column names on which to aggregate the input dataframe
-    margins     : list of list of column name indicating which grouping to aggregate. Can be empty in which case all grouping and subgrouping are aggregated.
-    var         : column to be aggregated
-    roudingBase : the rounding base. Has to be greater than 0.
+    df_in           : pandas dataframe
+    by              : list of column names on which to aggregate the input dataframe
+    margins         : list of list of column name indicating which grouping to aggregate. Can be empty in which case all grouping and subgrouping are aggregated. 
+    Controlling the rounding on a subset of margins will improve the run-time but will leave the other margins free to potentialy deviate far from their original values.
+    var             : column to be aggregated
+    roudingBase     : the rounding base. Has to be greater than 0.
+    fixRoundingDist : if an entry is close to a rounded value by p% of the rounding base, round that entry to it's closest rounded value and remove the other rounded value from consideration for that entry. 
+    This reduces the serach space and run time at the cost of the quality of the solution.
+    maxHeapSize     : the maximum size the heap. Has to be greater than 2. Default is 1000. 
+    A smaller heap will lead to faster run-time at the cost of the quality of the solution.
     
   output:
-    dataframe with columns listed in the "by" and "var" input parameters.
+    A dictionary with following keys:
+    input_table     : the original input data with columns listed in the "by" and "var" input parameters.
+    input_margins   : 
+    rounded_table   : the rounded solution of input data with columns listed in the "by" and "var" input parameters.
+    rounded_margins : 
+    objectives      : the objective functions value for the solution
+    opt_report      : a dictionary containing information about the optimisation process with folowing keys:
+      nIterations   : the number of partial solution expanded
+      nHeapPurges   : the number of times the heap was purged, keeping the best solution so far
+      nSolPurged    : the total number of partial solution that got purged and never further expanded
+    nCells          : the number of entries in the input table
+    nMargins        : the number of margin values from the input table 
+    nFixedCells     : the number of cells where the rounding is fixed and not subject to the optimisation process
+
   """
   
   nCells      = 0
