a89af23d9944643cd2606c32088cb038e2e7120b
Veozen <152989587+Veozen@users.noreply.github.com>
2024-11-15 21:31:38 -0500

    Update best_first_search.py

--- best_first_search.py
+++ best_first_search.py
@@ -16,18 +16,26 @@ def update_progress(progress ):
         progress = 1
         status = "Done...\r\n"
     block : int = int(round(barLength*progress))
-    text  : str = "\rProgress ".ljust(26) + " : " + "[{0}] {1}% {2}".format( "#"*block + "-"*(barLength-block), round(progress*100,2), status)
+    text  : str = "\rProgress ".ljust(10) + " : " + "[{0}] {1}% {2}".format( "#"*block + "-"*(barLength-block), round(progress*100,2), status)
     sys.stdout.write(text)
     sys.stdout.flush()
-    
 
-def best_first_search(possible_cell_values, initial_values, constraints, constraint_values, distance_funcs, n_solutions=0, max_heap_size=1000, reset_heap_fraction=0.75):
+
+def modify_margins(cell_id, previous_value, new_value, constraints_list, constraint_values ):
+  new_constraint_values  = constraint_values.copy()
+  for cons in constraints_list:
+    new_constraint_values[cons] = new_constraint_values[cons]-previous_value + new_value
+  return new_constraint_values
+
+
+def best_first_search(possible_cell_values, initial_values, constraints, cell_id_constraints, constraint_values, distance_funcs, n_solutions=0, max_heap_size=1000, reset_heap_fraction=0.75):
     """
     Performs best first search
     input:
       possible_cell_values  : dictionary of all decision variables along of all possible values for each
       initial_values        : dictionary of initial values for each decision variable
       constraints           : dictionary of each contraints to a list of decison variables that aggregate to that constraint's value
+      cell_id_constraints   : dictionary of each decision variables to a list of constraint to which it adds up.
       constraint_values     : dictionary of each contraints to the value they shopudl aggregate to
       n_solutions           : the number of solutions to output. The first solutions found.
       distance_funcs        : list of functions that will be used to calculation a lsit of distances to associate with a current (partial) solution
@@ -45,14 +53,17 @@ def best_first_search(possible_cell_values, initial_values, constraints, constra
     # the size of the heap after trimming
     reset_heap_size = int(reset_heap_fraction * max_heap_size)
     
+    # list of all decision variables
+    cell_id_list    = list(initial_values.keys())
+    
     # Priority queue for Best First Search
     pq              = []
     
     #the first solution  is the one where no decision has been made yet
     initial_partial_solution  = {}
-    param_list                = [initial_partial_solution, initial_values, constraints, constraint_values]
+    param_list                = [len(initial_partial_solution), initial_values, initial_partial_solution, constraint_values, constraint_values]
     initial_distances         = [f(*param_list) for f in distance_funcs]
-    initial_state             = (*initial_distances, counter, initial_partial_solution)
+    initial_state             = (*initial_distances, counter, initial_partial_solution, constraint_values)
     longest_partial_solution  = 0
     
     heapq.heappush(pq, initial_state)
@@ -60,48 +71,49 @@ def best_first_search(possible_cell_values, initial_values, constraints, constra
     Solutions = []
     while pq:
         current_best_node         = heapq.heappop(pq)
-        current_partial_solution  = current_best_node[-1]
-        current_counter           = current_best_node[-2]
+        current_partial_solution  = current_best_node[-2]
+        current_constraint_values = current_best_node[-1]
         current_distances         = current_best_node[:-2]
         
-        longest_partial_solution = max(longest_partial_solution, len(current_partial_solution))
+        longest_partial_solution  = max(longest_partial_solution, len(current_partial_solution))
         
         #update progress bar
         update_progress(longest_partial_solution/ len(initial_values)  )
         
         #if the partial solution is complete, store it with objective functions
         if len(current_partial_solution) == len(initial_values):
-          Solutions.append((*current_distances, current_partial_solution))
+          Solutions.append((*current_distances, current_partial_solution, current_constraint_values))
           
         # output the N first Solutions found
         if n_solutions > 0 and len(Solutions) == n_solutions:
           return Solutions, counter, n_heap_purges, n_sol_purged
           
+        #select a cellID to expland
+        cell_id                           = cell_id_list[len(current_partial_solution)]
+        current_partial_solution[cell_id] = initial_values[cell_id]
+        
         # Generate neighbors
-        for cell_id in possible_cell_values:
-            if cell_id not in current_partial_solution:
-              for value in possible_cell_values[cell_id]:
-                  new_partial_solution          = current_partial_solution.copy()
-                  new_partial_solution[cell_id] = value
-                  
-                  new_param_list                = [new_partial_solution,initial_values, constraints, constraint_values]
-                  new_distances                 = [f(*new_param_list) for f in distance_funcs]
-                  
-                  # a unique counter is stored in the state so that the heap will never attempt at comparing partial soutions distionaries as this would result in an error
-                  # if both distances are the same as another element in the heap, at least the counter will be different and used to order the elements
-                  counter                       += 1
-                  new_state                     = (*new_distances, counter, new_partial_solution)
-                  heapq.heappush(pq,new_state)
-              break
+        for value in possible_cell_values[cell_id]:
+            new_partial_solution          = current_partial_solution.copy()
+            
+            new_constraint_values         = modify_margins(cell_id, current_partial_solution[cell_id], value, cell_id_constraints[cell_id], current_constraint_values)
+            new_partial_solution[cell_id] = value
+            
+            new_param_list                = [len(new_partial_solution), initial_values, new_partial_solution, constraint_values, new_constraint_values]
+            new_distances                 = [f(*new_param_list) for f in distance_funcs]
+            
+            # a unique counter is stored in the state so that the heap will never attempt at comparing partial soutions distionaries as this would result in an error
+            # if both distances are the same as another element in the heap, at least the counter will be different and used to order the elements
+            counter                       += 1
+            new_state                     = (*new_distances, counter, new_partial_solution, new_constraint_values)
+            heapq.heappush(pq,new_state)
         
         #if heap gets too large, cut it in half keeping only the best partial solutions
-        if len(pq) > max_heap_size:
+        if len(pq) >= max_heap_size:
           pq.sort(key=lambda x: x[:nfuncs])
           n_sol_purged += len(pq) - reset_heap_size
-          pq = pq[:reset_heap_size]
+          pq            = pq[:reset_heap_size]
           heapq.heapify(pq)
           n_heap_purges += 1
-        
-        
+          
     return Solutions, counter, n_heap_purges, n_sol_purged
-
