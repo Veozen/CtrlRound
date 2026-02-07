86ac908f7165d121d81174f61f22f84c107201c2
Veozen <152989587+Veozen@users.noreply.github.com>
2026-02-07 14:23:07 -0500

    Update generate_table.py

--- CtrlRound/generate_table.py
+++ CtrlRound/generate_table.py
@@ -2,7 +2,7 @@ import pandas as pd
 import itertools
 import numpy as np
 
-def generate_random_table(n_dim,n_cat,scale=1):
+def generate_random_table(n_dim, n_cat, scale=1):
     #generate n_dim columns each with n_cat values
     sets = [set(range(n_cat)) for _ in range(n_dim)]
     cartesian_product = list(itertools.product(*sets))
@@ -10,4 +10,5 @@ def generate_random_table(n_dim,n_cat,scale=1):
     df["value"] = np.random.rand(len(df)) * scale
     return df
   
+
 
