7ab9d26f3cb70eff525d73f0ffe116dcdab667c0
Veozen <152989587+Veozen@users.noreply.github.com>
2026-02-07 18:33:45 -0500

    Update test_rounding.py

--- tests/test_rounding.py
+++ tests/test_rounding.py
@@ -1,1 +1,7 @@
+from CtrlRound import ctrl_round  
+from CtrlRound import generate_random_table  
 
+test = generate_random_table(3, 5, scale=2) 
+# Here we use the total distance (sum on the margins and interior cells) and we force the rounding of values that are 10% or less away from their bounds.  
+rounded = ctrl_round(test, by=[0,1,2], var="value", rounding_base=1, distance_total=True, fix_rounding_dist=0.1)  
+assert rounded not None
