7b21a46bcdbab6b6e3834b71700ad1608588e0b8
Veozen <152989587+Veozen@users.noreply.github.com>
2026-02-07 19:39:34 -0500

    Update __init__.py

--- CtrlRound/__init__.py
+++ CtrlRound/__init__.py
@@ -1,5 +1,5 @@
-from ctrl_round import ctrl_round.  
-from generate_table import generate_random_table 
+from ctrl_round import ctrl_round
+from generate_table import generate_random_table
 
 __author__ = "Christian Gagné"
 __version__ = "0.6.0"
