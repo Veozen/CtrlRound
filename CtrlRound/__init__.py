5e4526de8394001a317e3c06ca99878fabb590f7
Veozen <152989587+Veozen@users.noreply.github.com>
2026-02-07 19:34:26 -0500

    Update __init__.py

--- CtrlRound/__init__.py
+++ CtrlRound/__init__.py
@@ -1,5 +1,9 @@
-from .CtrlRound import ctrl_round
-from .generate_table import generate_random_table
+from . import CtrlRound  
+from . import generate_table 
+
+# Now pull the functions out of them
+ctrl_round = CtrlRound.ctrl_round
+generate_random_table = generate_table.generate_random_table
 
 __author__ = "Christian Gagné"
 __version__ = "0.6.0"
